0f55763cc6f67b75c19417e9cfe513ac6a2b63ba
Nate Finch <finchproservices@gmail.com>
2016-08-07 21:18:01 +0900

    fix: insecure link  weather api

--- services.js
+++ services.js
@@ -8,7 +8,7 @@ weatherApp.service('cityService', function() {
 weatherApp.service('weatherService', ['$resource', function($resource) {
 
     this.GetWeather = function(city, days) {
-        var weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily", { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
+        var weatherAPI = $resource("https://api.openweathermap.org/data/2.5/forecast/daily", { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
 
         return weatherResult = weatherAPI.get({ q: city, cnt: days, APPID: 'fce120f1c5f300d199b9f80a54754100' });
     };
